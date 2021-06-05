7ef9c1cfa23e5f731d479c851cf114a88abd9f83
Saiful Haqqi <saifulhq@gmail.com>
2021-06-05 13:53:11 +0700

    nodejs dummy json

---
+++ routes/oauth.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+const mod = require('../modules').module;
+const users = require('../database/users.json');
+
+/* GET home page. */
+router.get('/AuthPage', function (req, res, next) {
+    let state = mod.crypto.randomBytes(16).toString('hex');
+    res.cookie('XSRF-TOKEN', state);
+    res.send({ authUrl: "https://github.com/login/oauth/authorize?client_id=" + mod.config.CLIENT_ID + '&redirect_uri=' + mod.config.REDIRECT_URI + '&scope=read:user&allow_signup=' + true + '&state=' + state });
+
+});
+
+router.post('/getAccessToken', function (req, res) {
+    let state = req.headers["x-xsrf-token"];
+    mod.axios({
+        url: 'https://github.com/login/oauth/access_token?client_id=' + mod.config.CLIENT_ID + '&client_secret=' + mod.config.CLIENT_SECRET + '&code=' + req.body.code + '&redirect_uri=' + mod.config.REDIRECT_URI + '&state=' + state,
+        method: 'POST',
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(function (resp) {
+            console.log({ body: resp.data });
+            if (resp.data.access_token) {
+                req.session.token = resp.data.access_token;
+            }
+            res.send(resp.data);
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.send(err);
+        })
+
+});
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    console.log({ users });
+    const found = users.find(d => d.username == username && d.password == password);
+    console.log({ username, password, found });
+    if (found) {
+        res.send({ error: false, msg: "", data: Object.assign({}, found, { password: null }) });
+    } else {
+        res.send({ error: true, msg: "Username and password invalid" });
+    }
+});
+
+router.post('/getUserByToken', (req, res) => {
+    const { authToken, username } = req.body;
+    console.log({ authToken, username });
+    const found = users.find(d => {
+        if (d.username == username && d.authToken == authToken)
+            return true;
+        else
+            return false
+    })
+    if (found) {
+        res.send({ error: false, msg: "", data: Object.assign({}, found, { password: null }) });
+    } else {
+        res.send({ error: true, msg: "Username and token invalid" });
+    }
+})
+
+module.exports = router;
