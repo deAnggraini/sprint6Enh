0960000243c8f5a183cfeec96d17fd5d2001475e
Saiful Haqqi <saifulhq@gmail.com>
2021-06-10 02:11:05 +0700

    homepage - search

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -10,10 +10,13 @@ import { environment } from 'src/environments/environment';
 export class HomepageComponent implements OnInit {
 
   background_image: string;
+  background_category_menu: string;
+
   constructor(private theme: ThemeService) {
     const img = this.theme.getConfig().homepage.bg_img_top;
     const img_url = `${environment.apiUrl}/themes/homepage/${img}`;
     this.background_image = img_url;
+    this.background_category_menu = 'eclipse.svg';
   }
 
   ngOnInit(): void {
