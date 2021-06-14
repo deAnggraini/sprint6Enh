4b6f213e7c36b6b71ed6d7421c0a4c7eb2688635
Saiful Haqqi <saifulhq@gmail.com>
2021-06-15 01:22:58 +0700

    sesuaikan env dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,8 +7,8 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://pakar.ogya.co.id:8080/api',
-  backend_img: 'https://pakar.ogya.co.id:8080/images',
+  apiUrl: 'http://36.89.25.220:12080',
+  backend_img: 'http://36.89.25.220:12080/images',
 };
 
 /*
