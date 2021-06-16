9639fd2af86164909b17c5fba0a0b6261d0d8c2d
jumono <jumono@ogya.co.id>
2021-06-16 09:45:59 +0700

    add exception handling

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/authenticate/CustomAuthenticationProvider.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/authenticate/CustomAuthenticationProvider.java
@@ -1,19 +1,18 @@
 package id.co.bca.pakar.be.oauth2.authenticate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.common.exceptions.BadClientCredentialsException;
 import org.springframework.stereotype.Component;
 
 import id.co.bca.pakar.be.oauth2.dto.EaiLoginResponse;
@@ -27,30 +26,30 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 	@Autowired
 	private RemoteEaiAuthentication remoteEaiAuthentication;
 
+	@SuppressWarnings({ "deprecation", "deprecation" })
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		String name = authentication.getName();
 		String password = authentication.getCredentials().toString();
-
-		EaiLoginResponse response = remoteEaiAuthentication.authenticate();
-
 		logger.info("---- authentication ---- " + authentication);
 		if (name != null && password != null) {
+			logger.info("----- authenticate user to remote eai system -----");
+			EaiLoginResponse response = remoteEaiAuthentication.authenticate(name, password);
+
 			if (response.getOutputSchema().getStatus().equals("0")) {
 				logger.info("login success ");
-				final List<GrantedAuthority> grantedAuths = new ArrayList<>();
+				List<GrantedAuthority> grantedAuths = new ArrayList<>();
 				grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
 //			UserDetails principal = new SecUserDto(name, password, grantedAuths);
-				final Authentication auth = new UsernamePasswordAuthenticationToken(name, password,
-						Arrays.asList(new SimpleGrantedAuthority("ROLE_USER")));
+				final Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
 				return auth;
 			} else {
 				logger.info("user / password salah ");
-				return (Authentication) new BadClientCredentialsException();
-			}			
+				throw new BadCredentialsException("");
+			}
 		} else {
 			logger.info("user / password salah ");
-			return (Authentication) new BadClientCredentialsException();
+			throw new BadCredentialsException("");
 		}
 	}
 
