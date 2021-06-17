34d6bd1ae9fbd87f53df8a7f8c74bfba5752f3e4
Saiful Haqqi <saifulhq@gmail.com>
2021-06-17 22:48:27 +0700

    search result page

--- database/articles.js
+++ database/articles.js
@@ -1,6 +1,14 @@
 const articles = [
     {
         id: 1,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'SME' },
+            { id: 5, title: 'Time Loan' },
+        ],
         title: 'TAHAKA (Tahapan Berjangka)',
         short_desc: '',
         desc: 'Dalam melakukan pembukaan Tahaka beberapa dokumen penting yang harus dilengkapi antara lain adalah',
@@ -30,6 +38,14 @@ const articles = [
     },
     {
         id: 2,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'SME' },
+            { id: 5, title: 'Time Loan' },
+        ],
         title: 'Time Loan - SME',
         short_desc: '',
         desc: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
@@ -59,6 +75,14 @@ const articles = [
     },
     {
         id: 3,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'Komersial' },
+            { id: 5, title: 'Time Loan - Komersial' },
+        ],
         title: 'GIRO',
         short_desc: '',
         desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
@@ -88,6 +112,14 @@ const articles = [
     },
     {
         id: 4,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'Komersial' },
+            { id: 5, title: 'Time Loan - Korporasi' },
+        ],
         title: 'Kredit Local SME',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -117,6 +149,14 @@ const articles = [
     },
     {
         id: 5,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'Komersial' },
+            { id: 5, title: 'Time Loan - Korporasi' },
+        ],
         title: 'Tahapan',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -146,6 +186,11 @@ const articles = [
     },
     {
         id: 6,
+        type: 'pdf',
+        category: [
+            { id: 1, title: 'PAKAR PDF' },
+            { id: 2, title: 'Ketentuan Pengajuan Time Loan' },
+        ],
         title: 'Welma',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -175,6 +220,12 @@ const articles = [
     },
     {
         id: 7,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Lorem Ipsum' },
+            { id: 2, title: 'Lorem Ipsum' },
+            { id: 3, title: 'Lorem Ipsum' },
+        ],
         title: 'Deposito',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -204,6 +255,11 @@ const articles = [
     },
     {
         id: 8,
+        type: 'pdf',
+        category: [
+            { id: 1, title: 'PAKAR PDF' },
+            { id: 2, title: 'Ketentuan Kredit Local' },
+        ],
         title: 'Simpanan Pelajar',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -233,6 +289,13 @@ const articles = [
     },
     {
         id: 9,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Lorem Ipsum' },
+            { id: 2, title: 'Lorem Ipsum' },
+            { id: 3, title: 'Lorem Ipsum' },
+            { id: 3, title: 'Lorem Ipsum - Ipsum' },
+        ],
         title: 'Tahapan Xpresi',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -262,6 +325,13 @@ const articles = [
     },
     {
         id: 10,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Produk Untuk Nasabah' },
+            { id: 2, title: 'Produk Kredit' },
+            { id: 3, title: 'Kredit Produktif' },
+            { id: 4, title: 'Korporasi' },
+        ],
         title: 'TabunganKu',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -291,6 +361,12 @@ const articles = [
     },
     {
         id: 11,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Aplikasi Mesin' },
+            { id: 2, title: 'Pendukung Transaksi Umum' },
+            { id: 3, title: 'ABACAS' },
+        ],
         title: 'Tapres',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -320,7 +396,118 @@ const articles = [
     },
     {
         id: 12,
+        type: 'article',
+        category: [
+            { id: 1, title: 'Aplikasi Mesin' },
+            { id: 2, title: 'Pendukung Transaksi International' },
+            { id: 3, title: 'Andy' },
+        ],
         title: 'DUITT',
+        short_desc: '',
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        created_by: 'saifulh',
+        created_date: Date.parse('2021-01-30T10:48:00'),
+        updating_by: '',
+        updating_date: null,
+        update_by: '',
+        update_date: null,
+        img: 'duitt.jpeg',
+        attactments: [
+            {
+                file: '',
+            }
+        ],
+        histories: [
+            {
+                id: 1,
+                change_by: '',
+                change_date: '',
+                changes: [{
+                    id: 1,
+                    desc: ''
+                }]
+            }
+        ]
+    },
+    {
+        id: 13,
+        type: 'faq',
+        category: [
+            { id: 1, title: 'FAQ' },
+            { id: 2, title: 'FAQ Operasional' },
+            { id: 3, title: 'Studi Kasus Time Loan - SME' },
+        ],
+        title: 'Studi Kasus Time Loan - SME',
+        short_desc: '',
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        created_by: 'saifulh',
+        created_date: Date.parse('2021-01-30T10:48:00'),
+        updating_by: '',
+        updating_date: null,
+        update_by: '',
+        update_date: null,
+        img: 'duitt.jpeg',
+        attactments: [
+            {
+                file: '',
+            }
+        ],
+        histories: [
+            {
+                id: 1,
+                change_by: '',
+                change_date: '',
+                changes: [{
+                    id: 1,
+                    desc: ''
+                }]
+            }
+        ]
+    },
+    {
+        id: 14,
+        type: 'faq',
+        category: [
+            { id: 1, title: 'FAQ' },
+            { id: 2, title: 'FAQ Operasional' },
+            { id: 3, title: 'Studi Kasus Time Loan - Komersial' },
+        ],
+        title: 'Studi Kasus Time Loan - Komersial',
+        short_desc: '',
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        created_by: 'saifulh',
+        created_date: Date.parse('2021-01-30T10:48:00'),
+        updating_by: '',
+        updating_date: null,
+        update_by: '',
+        update_date: null,
+        img: 'duitt.jpeg',
+        attactments: [
+            {
+                file: '',
+            }
+        ],
+        histories: [
+            {
+                id: 1,
+                change_by: '',
+                change_date: '',
+                changes: [{
+                    id: 1,
+                    desc: ''
+                }]
+            }
+        ]
+    },
+    {
+        id: 15,
+        type: 'faq',
+        category: [
+            { id: 1, title: 'FAQ' },
+            { id: 2, title: 'Lorem Ipsum' },
+            { id: 3, title: 'Lorem Ipsum' },
+        ],
+        title: 'FAQ Lorem Ipsum',
         short_desc: '',
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         created_by: 'saifulh',
@@ -349,7 +536,7 @@ const articles = [
     },
 ]
 
-module.exports.search = [
+module.exports.suggestion = [
     {
         id: 1,
         parent: 'PAKAR',
--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const categoryArticle = require('../database/category-article');
-const { search, articles, recommendation, news, popular } = require('../database/articles');
+const { search, articles, recommendation, news, popular, suggestion } = require('../database/articles');
 const theme = require('../database/themes');
+const _ = require('lodash');
 
 router.get('/theme', function (req, res) {
     res.send({ error: false, msg: '', data: theme });
@@ -17,7 +18,44 @@ router.get('/all', (req, res) => {
 });
 
 router.post('/search', (req, res) => {
-    res.send({ error: false, msg: "", data: search });
+    const { keyword } = req.body;
+    if (!keyword || keyword == 'kosong') {
+        res.send({
+            error: false, msg: "", data: {
+                result: {
+                    total: 0, length: 0, data: []
+                }
+            }
+        });
+        return;
+    }
+    console.log({ keyword });
+    const keys = _.uniq(articles.map(d => d.type));
+    const grouping = _.groupBy(articles, 'type');
+    // console.log({ key, grouping });
+    const group = {};
+    for (let k of keys) {
+        group[k] = {
+            total: Math.floor(Math.random() * 100 + 10),
+            length: grouping[k].length,
+            data: grouping[k]
+        }
+    }
+    const data = {
+        result: {
+            data: articles,
+            total: Math.floor(Math.random() * 100 + 10),
+            length: articles.length
+        },
+        keys,
+        group,
+        suggestion: 'Pengajuan Time Loan SME'
+    }
+    res.send({ error: false, msg: "", data });
+});
+
+router.post('/suggestion', (req, res) => {
+    res.send({ error: false, msg: "", data: suggestion });
 });
 
 router.post('/recommendation', (req, res) => {
