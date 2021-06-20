74a552f1cc45f97008a082711aa15ceb7bcc7227
jumono <jumono@ogya.co.id>
2021-06-20 12:40:39 +0700

    implement logout API

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/AuthenticationController.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/AuthenticationController.java
@@ -2,12 +2,16 @@ package id.co.bca.pakar.be.oauth2.api;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,20 +53,24 @@ public class AuthenticationController extends BaseController {
 		}
 	}
 	
-//	@PostMapping(value = "/api/auth/logout", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
-//			MediaType.APPLICATION_JSON_VALUE })
-//	public String logout(HttpServletRequest request, HttpServletResponse response) {		
-//		String authHeader = request.getHeader("Authorization");
-//		logger.info("logout proces for token value " + authHeader);
-//        if (authHeader != null) {
-//            String tokenValue = authHeader.replace("Bearer", "").trim();
-//            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
-//            if(accessToken != null) {
-//            	tokenStore.removeAccessToken(accessToken);
-//            	return "0";
-//            } else
-//            	return "-1";
-//        }
-//        return "-1";
-//	}
+	@PostMapping(value = "/api/auth/logout", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	public String logout(HttpServletRequest request, HttpServletResponse response) {		
+		String authHeader = request.getHeader("Authorization");
+		try {
+			logger.info("logout proces for token value " + authHeader);
+			if (authHeader != null) {
+			    String tokenValue = authHeader.replace("Bearer", "").trim();
+			    Boolean logoutStatus = authenticationService.logout(tokenValue);
+			    if(logoutStatus) {
+			    	return "0";
+			    } else
+			    	return "-1";
+			}
+			return "-1";
+		} catch (Exception e) {
+			logger.error("exception",e);
+			return "-1";
+		}
+	}
 }
