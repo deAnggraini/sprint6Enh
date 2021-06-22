a652f6eebb6553c117dde5dbe5fe20daa620777d
jumono <jumono@ogya.co.id>
2021-06-22 10:38:19 +0700

    update

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/common/Constant.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/common/Constant.java
@@ -6,4 +6,9 @@ public class Constant {
 		public static final String LOGOUT_URL = "/api/auth/logout";
 		public static final String REFRESH_TOKEN_URL = "/api/auth/refreshToken";
 	}
+	
+	public static final class LoginStatus {
+		public static final String INCORRECT_USER_PASSWORD_CODE = "01"; 
+		public static final String INCORRECT_USER_PASSWORD_MESSAGE = "Incorrect User ID or password"; 
+	}
 }
