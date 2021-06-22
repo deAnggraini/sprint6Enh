2379657950cbfcd30ba89bca5e730b12a97f3014
jumono <jumono@ogya.co.id>
2021-06-22 09:16:17 +0700

    initial commit

---
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/common/Constant.java
@@ -0,0 +1,9 @@
+package id.co.bca.pakar.be.oauth2.common;
+
+public class Constant {
+	public static final class ApiEndpoint {
+		public static final String LOGIN_URL = "/api/auth/login";
+		public static final String LOGOUT_URL = "/api/auth/logout";
+		public static final String REFRESH_TOKEN_URL = "/api/auth/refreshToken";
+	}
+}
