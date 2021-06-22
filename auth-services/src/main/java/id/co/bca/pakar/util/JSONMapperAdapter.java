12f7da9a63a0194acf982fe4541d67aeba22d125
jumono <jumono@ogya.co.id>
2021-06-22 08:41:45 +0700

    initial commit

---
+++ auth-services/src/main/java/id/co/bca/pakar/util/JSONMapperAdapter.java
@@ -0,0 +1,52 @@
+package id.co.bca.pakar.util;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONMapperAdapter {
+	public static <T> String objectToJson(T obj) {
+		ObjectMapper mapper = new ObjectMapper();
+	    // Java object to JSON string
+	    String jsonString = null;
+		try {
+			jsonString = mapper.writeValueAsString(obj);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return jsonString;
+	}
+	
+	public static <T> Object jsonToObject(String str, Class clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		Object obj = null;
+		try {
+			obj = mapper.readValue(str, clazz);
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	
+	public static Object jsonToListObject(String str, Class clazz) {
+		ObjectMapper mapper = new ObjectMapper();
+		Object[] obj = null;
+		try {
+			obj = (Object[]) mapper.readValue(str, clazz);
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return obj;
+	}
+}
