f43a50b15c8b278faf802a640d359dd664ca026e
jumono <jumono@ogya.co.id>
2021-06-25 11:34:06 +0700

    initial commit and update

---
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/api/ArticleCategoryController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package id.co.bca.pakar.be.doc.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author OGYA
+ *
+ */
+@RestController
+public class ArticleCategoryController extends BaseController {
+	
+}
