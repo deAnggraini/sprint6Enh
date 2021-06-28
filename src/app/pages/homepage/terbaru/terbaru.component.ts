358e9d5974306b8345c2d16593b12a94eb4213e1
ayun <ayun@gitlab.lab>
2021-06-28 14:49:05 +0700

    responsive carousel

--- src/app/pages/homepage/terbaru/terbaru.component.ts
+++ src/app/pages/homepage/terbaru/terbaru.component.ts
@@ -18,7 +18,16 @@ export class TerbaruComponent implements OnInit {
     "slidesToScroll": 1,
     "infinite": false,
     "prevArrow": "<img class='a-left control-c prev slick-prev' src='./assets/media/svg/bca/homepage/carousel-prev.svg'>",
-    "nextArrow": "<img class='a-right control-c next slick-next' src='./assets/media/svg/bca/homepage/carousel-next.svg'>"
+    "nextArrow": "<img class='a-right control-c next slick-next' src='./assets/media/svg/bca/homepage/carousel-next.svg'>",
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   constructor(private articleService: ArticleService, private changeDetectorRef: ChangeDetectorRef) {
