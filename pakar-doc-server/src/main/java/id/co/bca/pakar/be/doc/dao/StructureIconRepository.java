f36c6d800c24055cefe345c402fa6627760831b3
jumono <jumono@ogya.co.id>
2021-06-29 15:08:24 +0700

    update

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureIconRepository.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureIconRepository.java
@@ -1,10 +1,12 @@
 package id.co.bca.pakar.be.doc.dao;
 
 import id.co.bca.pakar.be.doc.model.StructureIcons;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StructureIconRepository extends CrudRepository<StructureIcons, Long>{
-
+    @Query("SELECT m FROM StructureIcons m WHERE m.structure.id=:structureId AND m.deleted IS FALSE")
+    StructureIcons findByStructureId(Long structureId);
 }
