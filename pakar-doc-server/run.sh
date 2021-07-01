64880cfe77b45dbd88b9bd5f843d7bccee9503a6
root <root@devops.multisoft.co.id>
2021-07-01 16:23:00 +0700

    update from server

--- pakar-doc-server/run.sh
+++ pakar-doc-server/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clear
-java -jar -Dport=12082 -Dlogging.config=log4j2.xml -Dspring.profiles.active=dev-local -Dspring.config.location=application.yml target/pakar-doc-server.jar
+java -jar -Dport=13082 -Dlogging.config=log4j2.xml -Dspring.profiles.active=dev-ogya2 -Dspring.config.location=application.yml target/pakar-doc-server.jar
