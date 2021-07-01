ce02618a84be8c7b8ab22b1299e4e516fafc4b43
Dewi Anggraini <dewiku21@gmail.com>
2021-07-02 00:31:56 +0700

    update query api menu

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/dao/MenuRepository.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/dao/MenuRepository.java
@@ -19,12 +19,12 @@ public interface MenuRepository extends CrudRepository<Menu, String> {
     List<Long> findMenuId(@Param("username") String username);
 
     @Query(
-            value = "select m.* from r_menu m join r_menu_role rmr on m.id = rmr.menu_id join r_user_role rur on rmr.role_id=rur.role_id join r_user_profile rup on rur.username=:username where m.nav = 'top' AND m.deleted IS FALSE",
+            value = "select m.* from r_menu m left join r_menu_role rmr on m.id = rmr.menu_id left join r_user_role rur on rmr.role_id=rur.role_id left join r_user ru on rur.username=ru.username where ru.username=:username AND m.nav = 'top' AND m.deleted IS FALSE",
             nativeQuery = true)
     List<Menu> getAllTopMenuById(@Param("username") String username);
 
     @Query(
-            value = "select m.* from r_menu m join r_menu_role rmr on m.id = rmr.menu_id join r_user_role rur on rmr.role_id=rur.role_id join r_user_profile rup on rur.username=:username where m.nav = 'bottom' AND m.deleted IS FALSE",
+            value = "select m.* from r_menu m left join r_menu_role rmr on m.id = rmr.menu_id left join r_user_role rur on rmr.role_id=rur.role_id left join r_user ru on rur.username=ru.username where ru.username=:username AND m.nav = 'bottom' AND m.deleted IS FALSE",
             nativeQuery = true)
     List<Menu> getAllBottomMenuById(@Param("username") String username);
 
