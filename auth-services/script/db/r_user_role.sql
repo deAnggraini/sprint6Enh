e40d1a95bf521e1ce5dc89b40debf76e852996d2
Dewi Anggraini <dewiku21@gmail.com>
2021-07-01 07:51:22 +0700

    update data r_user_role

--- auth-services/script/db/r_user_role.sql
+++ auth-services/script/db/r_user_role.sql
@@ -1,1 +1,16 @@
-INSERT INTO r_user_role(created_by,created_date, modify_by, modify_date, deleted, user_id, role_id) VALUES ('READER', 'system', now()::TIMESTAMP, NULL, NULL, 'f','role reader' );
+INSERT INTO public.r_user_role
+(id, created_by, created_date, deleted, modify_by, modify_date, role_id, username)
+VALUES(1, 'system', now()::DATE, false, 'system', now()::DATE, 'SUPERADMIN', 'admin123');
+
+INSERT INTO public.r_user_role
+(id, created_by, created_date, deleted, modify_by, modify_date, role_id, username)
+VALUES(2, 'system', now()::DATE, false, 'system', now()::DATE, 'READER', 'reader12');
+
+INSERT INTO public.r_user_role
+(id, created_by, created_date, deleted, modify_by, modify_date, role_id, username)
+VALUES(3, 'system', now()::DATE, false, 'system', now()::DATE, 'EDITOR', 'editor12');
+
+INSERT INTO public.r_user_role
+(id, created_by, created_date, deleted, modify_by, modify_date, role_id, username)
+VALUES(3, 'system', now()::DATE, false, 'system', now()::DATE, 'PUBLISHER', 'publish1');
+COMMIT;
