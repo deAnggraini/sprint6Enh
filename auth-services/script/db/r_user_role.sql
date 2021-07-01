2e41a67b138232e359bc02cbec60885e4acf63b0
Dewi Anggraini <dewiku21@gmail.com>
2021-07-01 07:54:28 +0700

    update data r_user_role

--- auth-services/script/db/r_user_role.sql
+++ auth-services/script/db/r_user_role.sql
@@ -12,5 +12,5 @@ VALUES(3, 'system', now()::DATE, false, 'system', now()::DATE, 'EDITOR', 'editor
 
 INSERT INTO public.r_user_role
 (id, created_by, created_date, deleted, modify_by, modify_date, role_id, username)
-VALUES(3, 'system', now()::DATE, false, 'system', now()::DATE, 'PUBLISHER', 'publish1');
+VALUES(4, 'system', now()::DATE, false, 'system', now()::DATE, 'PUBLISHER', 'publish1');
 COMMIT;
