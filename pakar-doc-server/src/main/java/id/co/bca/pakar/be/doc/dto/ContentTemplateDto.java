68ba88a560405d067113de3ce4b2b0cd2e8e82b9
venty <venty@gitlab.lab>
2021-07-06 08:40:34 +0700

    add ArticleTemplateDto & ContentTemplateDto

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ContentTemplateDto.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ContentTemplateDto.java
@@ -1,2 +1,69 @@
-package id.co.bca.pakar.be.doc.dto;public class ContentTemplateDto {
+package id.co.bca.pakar.be.doc.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class ContentTemplateDto {
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("desc")
+    private String desc;
+    @JsonProperty("params")
+    private List<String> params;
+    @JsonProperty("children")
+    private List<String> children;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<String> getParams() {
+        return params;
+    }
+
+    public void setParams(List<String> params) {
+        this.params = params;
+    }
+
+    public List<String> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<String> children) {
+        this.children = children;
+    }
+
+    @Override
+    public String toString() {
+        return "ContentTemplateDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", desc='" + desc + '\'' +
+                ", params=" + params +
+                ", children=" + children +
+                '}';
+    }
 }
