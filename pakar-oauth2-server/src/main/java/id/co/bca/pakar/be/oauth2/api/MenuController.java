7f52e1b87b66e48f8ecb33435cd98038e1c1b795
jumono <jumono@ogya.co.id>
2021-07-06 10:28:24 +0700

    remove endpoint /api/auth/menu

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/MenuController.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/MenuController.java
@@ -1,21 +1,19 @@
 package id.co.bca.pakar.be.oauth2.api;
 
 import id.co.bca.pakar.be.oauth2.common.Constant;
-import org.springframework.web.bind.annotation.*;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import id.co.bca.pakar.be.oauth2.dto.MenuDto;
+import id.co.bca.pakar.be.oauth2.service.MenuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import id.co.bca.pakar.be.oauth2.dto.MenuDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.http.MediaType;
-import java.util.Arrays;
-import id.co.bca.pakar.be.oauth2.service.MenuService;
 
 @RestController
 @CrossOrigin
@@ -24,33 +22,6 @@ public class MenuController extends BaseController {
 
     @Autowired
     private MenuService menuService;
-
-//    @GetMapping(value = "/api/auth/menu", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
-//            MediaType.APPLICATION_JSON_VALUE })
-//    public ResponseEntity<RestResponse<MenuDto>> menu(@RequestHeader (name="Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username) {
-//        try {
-//            String tokenValue = "";
-//            if (authorization != null && authorization.contains("Bearer")) {
-//                tokenValue = authorization.replace("Bearer", "").trim();
-//                logger.info("token value request header --- "+tokenValue);
-//                logger.info("username request header --- "+username);
-//            }
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//            List<MenuDto> menu = menuService.getMenu(tokenValue, username);
-//            logger.info("menu controller " + menu.get(0));
-////            RestResponse<MenuDto> tResponse = new RestResponse(menu, "00", "Menu Success Load");
-////            return new ResponseEntity<RestResponse<MenuDto>>(tResponse, HttpStatus.OK);
-//            return this.createResponse(new MenuDto(), Constant.ApiResponseCode.MENU_PROFILE_SUCCESS.getAction()[0], Constant.ApiResponseCode.MENU_PROFILE_SUCCESS.getAction()[1]);
-//
-//        } catch (Exception e) {
-//            logger.error("exception", e);
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//            RestResponse<MenuDto> tResponse = new RestResponse(new MenuDto(), "01", "Menu Failed Load");
-//            return ResponseEntity.accepted().headers(headers).body(tResponse);
-//        }
-//    }
 
     @GetMapping("/api/auth/menu")
     public ResponseEntity<RestResponse<List<MenuDto>>> menus(@RequestHeader (name="Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username) {
@@ -62,7 +33,6 @@ public class MenuController extends BaseController {
                 logger.info("username request header --- "+username);
             }
             List<MenuDto> menu = menuService.getMenus(tokenValue, username);
-            logger.info("menu controller {}", menu.get(0));
             return this.createResponse(menu, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
         } catch (Exception e) {
             logger.error("exception", e);
