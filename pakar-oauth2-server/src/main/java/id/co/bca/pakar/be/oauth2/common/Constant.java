277cfe5d581057827e863596233e423333285082
jumono <jumono@ogya.co.id>
2021-07-08 09:54:55 +0700

    add new map error code

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/common/Constant.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/common/Constant.java
@@ -12,16 +12,22 @@ public class Constant {
 		// String argument
 		LOGIN_SUCCEED(new String[] { "00", "LOGIN SUCCESS" }),
 		INCORRECT_USERNAME_PASSWORD(new String[] { "01", "Incorrect User ID or password" }),
-		SESSION_HAS_EXPIRED(new String[] { "02", "Session has expired" }),
 		LOGOUT_SUCCEED(new String[] { "00", "User Has Logout" }),
 		LOGOUT_FAILED(new String[] { "01", "User Fail to Logout" }),
 		REFRESH_TOKEN_SUCCEED(new String[] { "00", "New Access Token Has Generated" }),
 		REFRESH_TOKEN_FAILED(new String[] { "01", "Fail to Generate new access token" }),
 		EXIST_USER_PROFILE(new String[] { "00", "User Profile Exist" }),
 		USER_PROFILE_NOT_FOUND(new String[] { "01", "User Profile Not Found" }),
-		MENU_PROFILE_SUCCESS(new String[] { "00", "Menu Success Load" }),
 		OK(new String[] { "00", "Response OK" }),
-		GENERAL_ERROR(new String[] { "01", "Undefined Error" });
+		GENERAL_ERROR(new String[] { "01", "Undefined Error" }),
+		ACCESS_TOKEN_EXPIRED(new String[] {"100", "Access Token Expired"}),
+		INVALID_TOKEN(new String[] {"101", "Invalid Token"}),
+		INVALID_ACCESS_TOKEN(new String[] {"102", "Invalid Access Token"}),
+		INVALID_REFRESH_TOKEN(new String[] {"103", "Invalid Refresh Token"}),
+		INVALID_GRANT(new String[] {"104", "Invalid Grant"}),
+		REFRESH_TOKEN_EXPIRED(new String[] {"105", "Invalid Refresh Token (expired)"}),
+		INVALID_CLIENT_ID(new String[] {"106", "Client not valid"}),
+        TOKEN_HAS_EXPIRED(new String[] {"107", "Token Has Expired"});
 
 		// declaring private variable for getting values
 		private String action[];
