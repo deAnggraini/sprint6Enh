87aaa18ec3da53d435a4153ad0ebce39796831cd
jumono <jumono@ogya.co.id>
2021-07-08 10:02:37 +0700

    disable cors

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/config/WebSecurityConfig.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/config/WebSecurityConfig.java
@@ -7,18 +7,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//	@Autowired
-//	private CustomAuthenticationProvider authenticationProvider;
-
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
@@ -38,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.anonymous().disable().csrf().disable().cors();
+        http.anonymous().disable().csrf().disable().cors().disable();
     }
 
     @Bean
@@ -54,9 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.authenticationProvider(authenticationProvider);
-        auth.userDetailsService(customUserDetailsService)
-                .passwordEncoder(userPasswordEncoder());
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(userPasswordEncoder());
     }
 
     @Override
@@ -66,14 +67,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 //	@Bean
-//	public RemoteEaiAuthentication getRemoteEaiAuthenticcation() {
-//		return new RemoteEaiAuthentication();
-//	}
-
-//	@Bean
 //    public CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 //        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
 //        configuration.setAllowedHeaders(Arrays.asList("Authorization", "X-USERNAME", "refreshToken"));
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
