0231641c1b6f767ea5b4b47cf3e075078bfd32c1
jumono <jumono@ogya.co.id>
2021-07-12 17:35:52 +0700

    update

--- pakar-doc-server/src/main/resources/script/data.sql
+++ pakar-doc-server/src/main/resources/script/data.sql
@@ -1,21 +1,29 @@
 ---- article Template ---
 INSERT INTO public.t_article_template
 (id, created_by, created_date, deleted, modify_by, modify_date, description, template_name, structure_id)
-VALUES(1, 'system',now()::DATE, 'f','system',now()::DATE, 'Pilih template ini jika template yang direkomendasikan kurang sesuai dengan atribut page yang akan Kamu buat.', 'Empty', null);
+VALUES(1, 'system',now()::DATE, 'f','system',now()::DATE, 'Pilih template ini jika template yang direkomendasikan kurang sesuai dengan atribut page yang akan Kamu buat.', 'Empty');
 INSERT INTO public.t_article_template
 (id, created_by, created_date, deleted, modify_by, modify_date, description, template_name, structure_id)
-VALUES(2, 'system',now()::DATE, 'f','system',now()::DATE, 'Pilih template ini jika template yang direkomendasikan kurang sesuai dengan atribut page yang akan Kamu buat.', 'Basic', 234);
+VALUES(2, 'system',now()::DATE, 'f','system',now()::DATE, 'Pilih template ini jika template yang direkomendasikan kurang sesuai dengan atribut page yang akan Kamu buat.', 'Basic');
+
+---- article Template ---
+--INSERT INTO public.t_article_template_structure
+--(id, created_by, created_date, deleted, modify_by, modify_date, description, template_name, structure_id)
+--VALUES(nextval('public.article_template_structure_seq'), 'system',now()::DATE, 'f','system',now()::DATE, 'Pilih template ini jika template yang direkomendasikan kurang sesuai dengan atribut page yang akan Kamu buat.', 'Empty', null);
+INSERT INTO public.t_article_template_structure
+(id, created_by, created_date, deleted, modify_by, modify_date, article_template_id, structure_id)
+VALUES(nextval('public.article_template_structure_seq'), 'system',now()::DATE, 'f','system',now()::DATE, 2, 234);
 
 ---- article Template Content ----
 INSERT INTO public.t_article_template_content
 (id, created_by, created_date, deleted, modify_by, modify_date, description, level, name, parent, sort, article_template_id)
-VALUES(1, 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi aturan/kaidah/ketetapan/syarat/kriteria atas produk/aplikasi yang harus dipahami pembaca sebelum melakukan prosedur atas produk/aplikasi tersebut; dapat dituangkan dalam bentuk kalimat ataupun tabel.', 1, 'Ketentuan Produk', null, 1, 2);
+VALUES(nextval('article_template_seq'), 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi aturan/kaidah/ketetapan/syarat/kriteria atas produk/aplikasi yang harus dipahami pembaca sebelum melakukan prosedur atas produk/aplikasi tersebut; dapat dituangkan dalam bentuk kalimat ataupun tabel.', 1, 'Ketentuan Produk', null, 1, 2);
 INSERT INTO public.t_article_template_content
 (id, created_by, created_date, deleted, modify_by, modify_date, description, level, name, parent, sort, article_template_id)
-VALUES(2, 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi proses/alur kerja/tahapan/cara kerja yang terkait atas suatu produk/aplikasi, biasanya dijelaskan dalam bentuk diagram alur.', 1, 'Prosedur  Produk', null, 2, 2);
+VALUES(nextval('article_template_seq'), 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi proses/alur kerja/tahapan/cara kerja yang terkait atas suatu produk/aplikasi, biasanya dijelaskan dalam bentuk diagram alur.', 1, 'Prosedur  Produk', null, 2, 2);
 INSERT INTO public.t_article_template_content
 (id, created_by, created_date, deleted, modify_by, modify_date, description, level, name, parent, sort, article_template_id)
-VALUES(4, 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi list-list formulir apa saja yang digunakan atas suatu produk/aplikasi.', 1, 'Formulir Produk', null, 3, 2);
+VALUES(nextval('article_template_seq'), 'system',now()::DATE, 'f','system',now()::DATE, 'Berisi list-list formulir apa saja yang digunakan atas suatu produk/aplikasi.', 1, 'Formulir Produk', null, 3, 2);
 
 
 --- r_images kebutuhan di r_template dan r_template_thumbnail---
