b726cc6932a2aaba1f6e0b9d049c47e7239e25b0
jumono <jumono@ogya.co.id>
2021-07-12 17:18:25 +0700

    update

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/service/UserService.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/service/UserService.java
@@ -2,7 +2,10 @@ package id.co.bca.pakar.be.oauth2.service;
 
 import id.co.bca.pakar.be.oauth2.dto.LoggedinDto;
 
+import java.util.List;
+
 public interface UserService {
 	LoggedinDto findUserByToken(String token);
 	LoggedinDto findUserByToken(String token, String username);
+	List<String> findRolesByUser(String username);
 }
