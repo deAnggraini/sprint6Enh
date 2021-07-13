1402c954d5c297931773735564eee47e8058559e
jumono <jumono@ogya.co.id>
2021-07-13 21:42:10 +0700

    add extends BaseDto

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ArticleTitleDto.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ArticleTitleDto.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-public class ArticleTitleDto {
+public class ArticleTitleDto extends BaseDto {
     @NotEmpty(message = "name is required")
     @Size(max = 50, message = "maximum length 50 characters")
     @Pattern(regexp="[0-9a-zA-Z\\/\\s\\-\\(\\)]*$", message = "title must alpha or numeric, whitespace, - , /, ()")
