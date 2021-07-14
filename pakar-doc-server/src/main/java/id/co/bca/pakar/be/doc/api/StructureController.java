5ea90e7108d8014345d35bf6628606623eff624e
jumono <jumono@ogya.co.id>
2021-07-14 20:08:20 +0700

    update

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/api/StructureController.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/api/StructureController.java
@@ -17,154 +17,172 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StructureController extends BaseController {
-	private static Logger logger = LoggerFactory.getLogger(StructureController.class);
+    private static Logger logger = LoggerFactory.getLogger(StructureController.class);
 
-	@Autowired
-	private StructureService structureService;
+    @Autowired
+    private StructureService structureService;
 
-	@Autowired
-	private StructureValidator structureValidator;
+    @Autowired
+    private StructureValidator structureValidator;
 
-	@Autowired
-	private MultiStructureValidator multiStructureValidator;
+    @Autowired
+    private MultiStructureValidator multiStructureValidator;
 
-	/**
-	 * add method add structure menu
-	 * for mutilpart/form-data, validator must using custom validator
-	 * @return
-	 */
-	@PostMapping(value = "/api/doc/saveStructure", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<RestResponse<StructureResponseDto>> saveStructure(@RequestHeader("Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username, @ModelAttribute StructureWithFileDto structure, BindingResult bindingResult) {
-		try {
-			logger.info("add structure process");
-			logger.info("validate request input");
-			structureValidator.validate(structure, bindingResult);
-			if (bindingResult.hasErrors()) {
-				logger.info("binding result "+bindingResult.getAllErrors());
-				String errMessage = "";
-				for (FieldError error : bindingResult.getFieldErrors()) {
-					errMessage = error.getDefaultMessage();
-				}
-				return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0], errMessage);
-			}
-			logger.info("name structure >> {}", structure.getName());
-			StructureResponseDto responseDto = structureService.add(username, structure);
-			return createResponse(responseDto, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
-		} catch (DataNotFoundException e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
-		} catch (InvalidLevelException e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
-		} catch (InvalidSortException e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_SORT_STRUCTURE.getAction()[0], Constant.ApiResponseCode.INVALID_SORT_STRUCTURE.getAction()[1]);
-		} catch (Exception e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
-		}
-	}
+    /**
+     * add method add structure menu
+     * for mutilpart/form-data, validator must using custom validator
+     *
+     * @return
+     */
+    @PostMapping(value = "/api/doc/saveStructure", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<StructureResponseDto>> saveStructure(@RequestHeader("Authorization") String authorization, @RequestHeader(name = "X-USERNAME") String username, @ModelAttribute StructureWithFileDto structure, BindingResult bindingResult) {
+        try {
+            logger.info("add structure process");
+            logger.info("validate request input");
+            structureValidator.validate(structure, bindingResult);
+            if (bindingResult.hasErrors()) {
+                logger.info("binding result " + bindingResult.getAllErrors());
+                List<String> errors = bindingResult
+                        .getFieldErrors()
+                        .stream()
+                        .map(x -> x.getDefaultMessage())
+                        .collect(Collectors.toList());
+                return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], errors.get(0));
+            }
+            logger.info("name structure >> {}", structure.getName());
+            StructureResponseDto responseDto = structureService.add(username, structure);
+            return createResponse(responseDto, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
+        } catch (DataNotFoundException e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
+        } catch (InvalidLevelException e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
+        } catch (InvalidSortException e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_SORT_STRUCTURE.getAction()[0], Constant.ApiResponseCode.INVALID_SORT_STRUCTURE.getAction()[1]);
+        } catch (Exception e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
+        }
+    }
 
-	/**
-	 * add method update structure menu
-	 * @return
-	 */
-	@PostMapping(value = "/api/doc/updateStructure", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<RestResponse<StructureResponseDto>> updateStructure(@RequestHeader("Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username, @ModelAttribute StructureWithFileDto structure, BindingResult bindingResult) {
-		try {
-			structureValidator.validate(structure, bindingResult);
-			if(bindingResult.hasErrors()) {
-				logger.info("binding result "+bindingResult.getAllErrors());
-				return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0], Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[1]);
-			}
-			StructureResponseDto response = structureService.update(username, structure);
-			return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
-		} catch (DataNotFoundException e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
-		} catch (InvalidLevelException e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
-		} catch (Exception e) {
-			logger.error("exception", e);
-			return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
-		}
-	}
+    /**
+     * add method update structure menu
+     *
+     * @return
+     */
+    @PostMapping(value = "/api/doc/updateStructure", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<StructureResponseDto>> updateStructure(@RequestHeader("Authorization") String authorization, @RequestHeader(name = "X-USERNAME") String username, @ModelAttribute StructureWithFileDto structure, BindingResult bindingResult) {
+        try {
+            structureValidator.validate(structure, bindingResult);
+            if (bindingResult.hasErrors()) {
+                logger.info("binding result " + bindingResult.getAllErrors());
+//				return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0], Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[1]);
+                List<String> errors = bindingResult
+                        .getFieldErrors()
+                        .stream()
+                        .map(x -> x.getDefaultMessage())
+                        .collect(Collectors.toList());
+                return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], errors.get(0));
+            }
+            StructureResponseDto response = structureService.update(username, structure);
+            return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
+        } catch (DataNotFoundException e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
+        } catch (InvalidLevelException e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
+        } catch (Exception e) {
+            logger.error("exception", e);
+            return createResponse(new StructureResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
+        }
+    }
 
-	/**
-	 * add method edit structure menu
-	 * @return
-	 */
-	@PostMapping(value = "/api/doc/deleteStructure", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<RestResponse<DeleteStructureDto>> deleteStructure(@RequestHeader("Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username, @Valid @RequestBody DeleteStructureDto structures) {
-		try {
-			logger.info("deleting structure process");
-			DeleteStructureDto response = structureService.delete(username, structures);
-			return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
-		} catch (DataNotFoundException e) {
-			logger.error("exception", e);
-			return createResponse(new DeleteStructureDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
-		} catch (Exception e) {
-			logger.error("exception", e);
-			return createResponse(new DeleteStructureDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
-		}
-	}
+    /**
+     * add method edit structure menu
+     *
+     * @return
+     */
+    @PostMapping(value = "/api/doc/deleteStructure", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<DeleteStructureDto>> deleteStructure(@RequestHeader("Authorization") String authorization, @RequestHeader(name = "X-USERNAME") String username, @Valid @RequestBody DeleteStructureDto structures) {
+        try {
+            logger.info("deleting structure process");
+            DeleteStructureDto response = structureService.delete(username, structures);
+            return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
+        } catch (DataNotFoundException e) {
+            logger.error("exception", e);
+            return createResponse(new DeleteStructureDto(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
+        } catch (Exception e) {
+            logger.error("exception", e);
+            return createResponse(new DeleteStructureDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
+        }
+    }
 
-	/**
-	 * saveing batch structures
-	 * @param authorization
-	 * @param username
-	 * @param structures
-	 * @return
-	 */
-	@PostMapping(value = "/api/doc/saveBatchStructure", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<RestResponse<List<StructureResponseDto>>> saveBatchStructure(@RequestHeader("Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username, @RequestBody List<StructureWithFileDto> structures, BindingResult bindingResult) {
-		try {
-			multiStructureValidator.validate(structures, bindingResult);
-			if(bindingResult.hasErrors()) {
-				logger.info("binding result "+bindingResult.getAllErrors());
-				return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0], Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[1]);
-			}
-			List<StructureResponseDto> response = structureService.saveBatchStructures(username, structures);
-			return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
-		} catch (DataNotFoundException e) {
-			logger.error("exception", e);
-			return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
-		} catch (InvalidLevelException e) {
-			logger.error("exception", e);
-			return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
-		} catch (Exception e) {
-			logger.error("exception", e);
-			return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
-		}
-	}
+    /**
+     * saveing batch structures
+     *
+     * @param authorization
+     * @param username
+     * @param structures
+     * @return
+     */
+    @PostMapping(value = "/api/doc/saveBatchStructure", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<List<StructureResponseDto>>> saveBatchStructure(@RequestHeader("Authorization") String authorization, @RequestHeader(name = "X-USERNAME") String username, @RequestBody List<StructureWithFileDto> structures, BindingResult bindingResult) {
+        try {
+            multiStructureValidator.validate(structures, bindingResult);
+            if (bindingResult.hasErrors()) {
+                logger.info("binding result " + bindingResult.getAllErrors());
+//				return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0], Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[1]);
+                List<String> errors = bindingResult
+                        .getFieldErrors()
+                        .stream()
+                        .map(x -> x.getDefaultMessage())
+                        .collect(Collectors.toList());
+                return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], errors.get(0));
+            }
+            List<StructureResponseDto> response = structureService.saveBatchStructures(username, structures);
+            return createResponse(response, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
+        } catch (DataNotFoundException e) {
+            logger.error("exception", e);
+            return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[0], Constant.ApiResponseCode.DATA_NOT_FOUND.getAction()[1]);
+        } catch (InvalidLevelException e) {
+            logger.error("exception", e);
+            return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[0], Constant.ApiResponseCode.INVALID_STRUCTURE_LEVEL.getAction()[1]);
+        } catch (Exception e) {
+            logger.error("exception", e);
+            return createResponse(new ArrayList<StructureResponseDto>(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
+        }
+    }
 
-	/**
-	 * get all structure
-	 * @return
-	 */
-	@GetMapping("/api/doc/category")
-	public ResponseEntity<RestResponse<List<MenuDto>>> getCategories(@RequestHeader("Authorization") String authorization, @RequestHeader (name="X-USERNAME") String username) {
-		logger.info("get all structure/category");
-		try {
-			List<MenuDto> menus = structureService.getCategories(username, getTokenFromHeader(authorization));
-			return createResponse(menus,Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
-		} catch (Exception e) {
-			logger.error("exception", e);
-			return createResponse(new ArrayList<MenuDto>(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
-		}
-	}
+    /**
+     * get all structure
+     *
+     * @return
+     */
+    @GetMapping("/api/doc/category")
+    public ResponseEntity<RestResponse<List<MenuDto>>> getCategories(@RequestHeader("Authorization") String authorization, @RequestHeader(name = "X-USERNAME") String username) {
+        logger.info("get all structure/category");
+        try {
+            List<MenuDto> menus = structureService.getCategories(username, getTokenFromHeader(authorization));
+            return createResponse(menus, Constant.ApiResponseCode.OK.getAction()[0], Constant.ApiResponseCode.OK.getAction()[1]);
+        } catch (Exception e) {
+            logger.error("exception", e);
+            return createResponse(new ArrayList<MenuDto>(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], Constant.ApiResponseCode.GENERAL_ERROR.getAction()[1]);
+        }
+    }
 }
--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/ControllerExceptionHandler.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/ControllerExceptionHandler.java
@@ -6,41 +6,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
-	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		List<String> errors = new ArrayList<String>();
-		String errMessage = "";
-		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-//			errors.add(error.getField() + ": " + error.getDefaultMessage());
-			errors.add(error.getDefaultMessage());
-			errMessage = error.getDefaultMessage();
-		}
-//		for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
-//			errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
-//		}
-		logger.debug("validate failed with error {}", errors);
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+        String errMessage = errors.get(0);
+        logger.debug("validate failed with error {}", errors);
         HashMap<String, String> responseStatus = new HashMap<>();
         responseStatus.put("code", Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[0]);
 //		responseStatus.put("message", Constant.ApiResponseCode.REQUEST_PARAM_INVALID.getAction()[1]);
-		responseStatus.put("message", errMessage);
+        responseStatus.put("message", errMessage);
 
-		Map<String, Object> body = new LinkedHashMap<>();
+        Map<String, Object> body = new LinkedHashMap<>();
         body.put("data", 0);
         body.put("status", responseStatus);
 
-		return new ResponseEntity<>(body, headers, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+    }
 }
