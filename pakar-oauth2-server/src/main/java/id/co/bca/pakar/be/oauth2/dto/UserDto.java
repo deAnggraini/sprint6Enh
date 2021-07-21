e5f4e6734b088e841ca639b053914e15a59b9e44
jumono <jumono@ogya.co.id>
2021-07-21 10:44:43 +0700

    update

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/dto/UserDto.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/dto/UserDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class UserDto {	
+public class UserDto {
 	@JsonProperty("username")
 	private String username;
 	@JsonProperty("password")
@@ -25,19 +25,10 @@ public class UserDto {
 	private String companyName;
 	@JsonProperty("phone")
 	private String phone;
-	@JsonProperty("authToken")
-	private String access_token;
-	@JsonInclude(JsonInclude.Include.NON_NULL) 
-	private String token_type;
-	@JsonProperty("expiresIn")
-	private String expires_in;
-	@JsonProperty("refreshToken")
-	private String refresh_token;
-	@JsonInclude(JsonInclude.Include.NON_NULL) 
-	private String scope;
+	@JsonProperty("pic")
+	private String picture = "./assets/media/users/default.jpg"; // default value
 	@JsonProperty("roles")
 	private List<String> roleDtos = new ArrayList<String>();
-	
 	public String getUsername() {
 		return username;
 	}
@@ -47,7 +38,6 @@ public class UserDto {
 	public String getFullname() {
 		return fullname;
 	}
-	
 	public String getPassword() {
 		return password;
 	}
@@ -93,37 +83,14 @@ public class UserDto {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	public String getAccess_token() {
-		return access_token;
+	public String getPicture() {
+		return picture;
 	}
-	public void setAccess_token(String access_token) {
-		this.access_token = access_token;
+
+	public void setPicture(String picture) {
+		this.picture = picture;
 	}
-	public String getToken_type() {
-		return token_type;
-	}
-	public void setToken_type(String token_type) {
-		this.token_type = token_type;
-	}
-	public String getExpires_in() {
-		return expires_in;
-	}
-	public void setExpires_in(String expires_in) {
-		this.expires_in = expires_in;
-	}
-	public String getRefresh_token() {
-		return refresh_token;
-	}
-	public void setRefresh_token(String refresh_token) {
-		this.refresh_token = refresh_token;
-	}
-	public String getScope() {
-		return scope;
-	}
-	public void setScope(String scope) {
-		this.scope = scope;
-	}
+
 	public List<String> getRoleDtos() {
 		return roleDtos;
 	}
