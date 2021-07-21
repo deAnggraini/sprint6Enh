9c45ac06afbc7d246e38a6d34d7e775ac1defe67
jumono <jumono@ogya.co.id>
2021-07-21 11:05:24 +0700

    change message from resource message

--- pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/AuthenticationController.java
+++ pakar-oauth2-server/src/main/java/id/co/bca/pakar/be/oauth2/api/AuthenticationController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @RestController
 @CrossOrigin
@@ -31,31 +32,30 @@ public class AuthenticationController extends BaseController {
 			LoggedinDto oAuthToken = authenticationService.authenticate(dto);
 			oAuthToken.setRememberMe(dto.getRememberMe());
 			
-			return this.createResponse(oAuthToken, Constant.ApiResponseCode.LOGIN_SUCCEED.getAction()[0], Constant.ApiResponseCode.LOGIN_SUCCEED.getAction()[1]);
+			return this.createResponse(oAuthToken, Constant.ApiResponseCode.OK.getAction()[0], messageSource.getMessage("success.response", null, new Locale("en", "US")));
 		} catch (Exception e) {
 			logger.error("exception", e);
-			return this.createResponse(new LoggedinDto(), Constant.ApiResponseCode.INCORRECT_USERNAME_PASSWORD.getAction()[0], Constant.ApiResponseCode.INCORRECT_USERNAME_PASSWORD.getAction()[1]);
+			return this.createResponse(new LoggedinDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], messageSource.getMessage("userid.password.incorrect", null, new Locale("en", "US")));
 		}
 	}
 	
 	@PostMapping(value = Constant.ApiEndpoint.LOGOUT_URL, consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
 			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<RestResponse<String>> logout(@RequestHeader("Authorization") String authorization) {
-//		String authHeader = request.getHeader("Authorization");
 		try {
 			logger.info("logout proces for token value " + authorization);
 			if (authorization != null) {
 			    String tokenValue = authorization.replace("Bearer", "").trim();
 			    Boolean logoutStatus = authenticationService.logout(tokenValue);
 			    if(logoutStatus) {
-			    	return this.createResponse("0", Constant.ApiResponseCode.LOGOUT_SUCCEED.getAction()[0], Constant.ApiResponseCode.LOGOUT_SUCCEED.getAction()[1]);
+			    	return this.createResponse("0", Constant.ApiResponseCode.OK.getAction()[0], messageSource.getMessage("logout.success", null, new Locale("en", "US")));
 			    } else
-			    	return this.createResponse("-1", Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[0], Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[1]);			    
+			    	return this.createResponse("-1", Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], messageSource.getMessage("logout.failed", null, new Locale("en", "US")));
 			}
-			return this.createResponse("-1", Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[0], Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[1]);
+			return this.createResponse("-1", Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], messageSource.getMessage("logout.failed", null, new Locale("en", "US")));
 		} catch (Exception e) {
 			logger.error("exception",e);
-			return this.createResponse("-1", Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[0], Constant.ApiResponseCode.LOGOUT_FAILED.getAction()[1]);
+			return this.createResponse("-1", Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], messageSource.getMessage("logout.failed", null, new Locale("en", "US")));
 		}
 	}
 	
@@ -65,10 +65,10 @@ public class AuthenticationController extends BaseController {
 		try {
 			logger.info("generate new access token with refresh token --- " + dto.getRefreshToken());
 			RefreshTokenResponseDto oAuthToken = authenticationService.generateNewAccessToken(dto.getRefreshToken());
-			return this.createResponse(oAuthToken, Constant.ApiResponseCode.REFRESH_TOKEN_SUCCEED.getAction()[0], Constant.ApiResponseCode.REFRESH_TOKEN_SUCCEED.getAction()[1]);
+			return this.createResponse(oAuthToken, Constant.ApiResponseCode.OK.getAction()[0], messageSource.getMessage("refreshtoken.success", null, new Locale("en", "US")));
 		} catch (Exception e) {
 			logger.error("exception", e);
-			return this.createResponse(new RefreshTokenResponseDto(), Constant.ApiResponseCode.REFRESH_TOKEN_FAILED.getAction()[0], Constant.ApiResponseCode.REFRESH_TOKEN_FAILED.getAction()[1]);
+			return this.createResponse(new RefreshTokenResponseDto(), Constant.ApiResponseCode.GENERAL_ERROR.getAction()[0], messageSource.getMessage("refreshtoken.failed", null, new Locale("en", "US")));
 		}
 	}
 }
