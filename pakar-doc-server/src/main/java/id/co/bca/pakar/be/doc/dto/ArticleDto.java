2613e5dac0126aa7660cd6edd0b5e5cdca5a8996
jumono <jumono@ogya.co.id>
2021-07-21 08:45:20 +0700

    update

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ArticleDto.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/ArticleDto.java
@@ -1,9 +1,41 @@
 package id.co.bca.pakar.be.doc.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import id.co.bca.pakar.be.doc.model.SkRefference;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArticleDto extends BaseArticleDto {
+    @JsonProperty("desc")
+    private String shortDescription;
+
+    @JsonProperty("image")
     private MultipartFile image;
+
+    @JsonProperty("video")
+    private String videoLink;
+
+    @JsonProperty("contents")
+    private List<ArticleContentDto> contents = new ArrayList<>();
+
+    @JsonProperty("references")
+    private List<SkRefference> refferences = new ArrayList<>();
+
+    @JsonProperty("related")
+    private List<RelatedArticleDto> related = new ArrayList<>();
+
+    @JsonProperty("suggestions")
+    private List<SuggestionArticleDto> suggestions = new ArrayList<>();
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
 
     public MultipartFile getImage() {
         return image;
@@ -11,5 +43,45 @@ public class ArticleDto extends BaseArticleDto {
 
     public void setImage(MultipartFile image) {
         this.image = image;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+
+    public List<ArticleContentDto> getContents() {
+        return contents;
+    }
+
+    public void setContents(List<ArticleContentDto> contents) {
+        this.contents = contents;
+    }
+
+    public List<SkRefference> getRefferences() {
+        return refferences;
+    }
+
+    public void setRefferences(List<SkRefference> refferences) {
+        this.refferences = refferences;
+    }
+
+    public List<RelatedArticleDto> getRelated() {
+        return related;
+    }
+
+    public void setRelated(List<RelatedArticleDto> related) {
+        this.related = related;
+    }
+
+    public List<SuggestionArticleDto> getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(List<SuggestionArticleDto> suggestions) {
+        this.suggestions = suggestions;
     }
 }
--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/BaseArticleDto.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/BaseArticleDto.java
@@ -2,22 +2,9 @@ package id.co.bca.pakar.be.doc.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.*;
-
 public class BaseArticleDto extends ArticleTitleDto {
     @JsonProperty("id")
     protected Long id;
-    @NotEmpty(message = "desc is required")
-    @JsonProperty("usedBy")
-    protected String usedBy;
-    @NotNull(message = "structure id is required")
-    @Min(value = 1, message = "minimum value is 1")
-    @JsonProperty("lokasi")
-    protected Long structureId;
-    @NotNull(message = "template id required")
-    @Min(value = 1, message = "minimum value is 1")
-    @JsonProperty("templateId")
-    protected Long templateId;
 
     public Long getId() {
         return id;
@@ -25,48 +12,5 @@ public class BaseArticleDto extends ArticleTitleDto {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getJudulArticle() {
-        return judulArticle;
-    }
-
-    public void setJudulArticle(String judulArticle) {
-        this.judulArticle = judulArticle;
-    }
-
-    public String getUsedBy() {
-        return usedBy;
-    }
-
-    public void setUsedBy(String usedBy) {
-        this.usedBy = usedBy;
-    }
-
-    public Long getStructureId() {
-        return structureId;
-    }
-
-    public void setStructureId(Long structureId) {
-        this.structureId = structureId;
-    }
-
-    public Long getTemplateId() {
-        return templateId;
-    }
-
-    public void setTemplateId(Long templateId) {
-        this.templateId = templateId;
-    }
-
-    @Override
-    public String toString() {
-        return "BaseArticleDto{" +
-                "id=" + id +
-                ", judulArticle='" + judulArticle + '\'' +
-                ", usedBy='" + usedBy + '\'' +
-                ", structureId=" + structureId +
-                ", templateId=" + templateId +
-                '}';
     }
 }
