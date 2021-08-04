967409e84203040e6d667cd07ecfd6d7ed1df46e
ayun <ayun@gitlab.lab>
2021-08-04 18:40:14 +0700

    modify terbaru & recommendation

--- src/app/modules/article/recommendation/recommendation.component.ts
+++ src/app/modules/article/recommendation/recommendation.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ArticleService } from '../../_services/article.service';
 import { environment } from 'src/environments/environment';
+import { PaginationModel } from 'src/app/utils/_model/pagination';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-recommendation',
@@ -16,24 +18,34 @@ export class RecommendationComponent implements OnInit {
   backend_img: string = environment.backend_img;
 
   // pagination
-  dataPerPage: number = 6;
+  dataPerPage: number = 12;
   page: number = 1;
+  rowPage: number = 3;
+  paging: PaginationModel = PaginationModel.createEmpty();
+
 
   constructor(
     private router: Router,
     private articleService: ArticleService,
-    private changeDetectorRef: ChangeDetectorRef) { }
+    private changeDetectorRef: ChangeDetectorRef,
+    private location: Location) { }
 
   ngOnInit(): void {
     this.articleService.recommendation().subscribe(
       resp => {
+        // this.dataList = resp;
         this.dataList = resp.slice(0, 12);
-        this.total = Math.floor(Math.random() * 100 + 10);
+        // this.total = Math.floor(Math.random() * 100 + 10);
         this.length = resp.length;
         setTimeout(() => this.changeDetectorRef.detectChanges(), 0);
 
+        this.paging = new PaginationModel(this.page, this.length, this.dataPerPage, this.rowPage);
       }
     );
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
--- src/app/modules/article/terbaru/terbaru.component.ts
+++ src/app/modules/article/terbaru/terbaru.component.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 import { ArticleService } from '../../_services/article.service';
 import { Location } from '@angular/common';
+import { PaginationModel } from 'src/app/utils/_model/pagination';
 
 @Component({
   selector: 'app-terbaru',
@@ -16,6 +17,12 @@ export class TerbaruComponent implements OnInit {
   length: number = 0;
   backend_img: string = environment.backend_img;
 
+  // pagination
+  dataPerPage: number = 12;
+  page: number = 1;
+  rowPage: number = 3;
+  paging: PaginationModel = PaginationModel.createEmpty();
+
   constructor(
     private router: Router,
     private articleService: ArticleService,
@@ -24,12 +31,13 @@ export class TerbaruComponent implements OnInit {
 
   ngOnInit(): void {
     this.articleService.news().subscribe(
-      resp => {
-        this.dataList = resp;
-        this.total = Math.floor(Math.random() * 100 + 10);
+      resp => {        
+        this.dataList = resp.slice(0, 12);
+        // this.total = Math.floor(Math.random() * 100 + 10);
         this.length = resp.length;
         setTimeout(() => this.changeDetectorRef.detectChanges(), 0);
 
+        this.paging = new PaginationModel(this.page, this.length, this.dataPerPage, this.rowPage);
       }
     );
   }
