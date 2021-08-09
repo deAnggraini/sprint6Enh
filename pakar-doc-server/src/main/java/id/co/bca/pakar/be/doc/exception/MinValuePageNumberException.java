7b4b79ea255f21dca5955d4da4012c516e7a9d49
jumono <jumono@ogya.co.id>
2021-08-09 15:21:11 +0700

    update

---
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/MinValuePageNumberException.java
@@ -0,0 +1,11 @@
+package id.co.bca.pakar.be.doc.exception;
+
+public class MinValuePageNumberException extends Exception {
+    public MinValuePageNumberException(String message) {
+        super(message);
+    }
+
+    public MinValuePageNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
