a3a4c50c2f279ed709313c6c64c29a0a24320d48
jumono <jumono@ogya.co.id>
2021-08-11 13:33:40 +0700

    initial commit

---
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/ArticleEditRepository.java
@@ -0,0 +1,18 @@
+package id.co.bca.pakar.be.doc.dao;
+
+import id.co.bca.pakar.be.doc.model.ArticleEdit;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleEditRepository extends CrudRepository<ArticleEdit, Long> {
+    @Query(
+        "SELECT m FROM ArticleEdit m " +
+                "WHERE m.article.id=:articleId " +
+                "AND m.status IS TRUE " +
+                "AND m.deleted IS FALSE "
+    )
+    Iterable<ArticleEdit> findArticleInEditingStatus(@Param("articleId") Long articleId);
+}
