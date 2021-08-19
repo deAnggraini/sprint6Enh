f88ac66811f414e08be24141f6cc7994f4f5039c
jumono <jumono@ogya.co.id>
2021-08-19 15:45:05 +0700

    update

---
+++ pakar-wf-server/src/main/java/id/co/bca/pakar/be/wf/dto/auth/UserProfileDto.java
@@ -0,0 +1,149 @@
+package id.co.bca.pakar.be.wf.dto.auth;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserProfileDto {
+	@JsonProperty("username")
+	private String username;
+	@JsonProperty("password")
+	private String password;
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("fullname")
+	private String fullname;
+	@JsonProperty("firstname")
+	private String firstname;
+	@JsonProperty("lastname")
+	private String lastname;
+	@JsonProperty("accupation")
+	private String occupation;
+	@JsonProperty("companyName")
+	private String companyName;
+	@JsonProperty("phone")
+	private String phone;
+	@JsonProperty("pic")
+	private String picture = "./assets/media/users/default.jpg"; // default value
+	@JsonProperty("remember")
+	private Boolean rememberMe = Boolean.TRUE; 
+	@JsonProperty("authToken")
+	private String access_token;
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
+	private String token_type;
+	@JsonProperty("expiresIn")
+	private String expires_in;
+	@JsonProperty("refreshToken")
+	private String refresh_token;
+	@JsonInclude(JsonInclude.Include.NON_NULL) 
+	private String scope;
+	@JsonProperty("roles")
+	private List<String> roleDtos = new ArrayList<String>();
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public Boolean getRememberMe() {
+		return rememberMe;
+	}
+	public void setRememberMe(Boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+	public String getAccess_token() {
+		return access_token;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	public String getToken_type() {
+		return token_type;
+	}
+	public void setToken_type(String token_type) {
+		this.token_type = token_type;
+	}
+	public String getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(String expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+	public String getScope() {
+		return scope;
+	}
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+	public List<String> getRoleDtos() {
+		return roleDtos;
+	}
+	public void setRoleDtos(List<String> roleDtos) {
+		this.roleDtos = roleDtos;
+	}
+}
