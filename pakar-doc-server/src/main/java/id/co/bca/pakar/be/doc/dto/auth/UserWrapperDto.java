ef026c4d2843ce6e959d46ff5d735b902aabd6cd
jumono <jumono@ogya.co.id>
2021-08-24 19:41:51 +0700

    initial commit

---
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dto/auth/UserWrapperDto.java
@@ -0,0 +1,13 @@
+package id.co.bca.pakar.be.doc.dto.auth;
+
+public class UserWrapperDto {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
