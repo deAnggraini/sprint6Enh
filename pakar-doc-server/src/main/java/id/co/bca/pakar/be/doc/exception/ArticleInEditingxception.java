4c203ad741aefa03122c8d96ea76e8956d7a0d6b
jumono <jumono@ogya.co.id>
2021-08-25 21:45:22 +0700

    initial commit

---
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/ArticleInEditingxception.java
@@ -0,0 +1,11 @@
+package id.co.bca.pakar.be.doc.exception;
+
+public class ArticleInEditingxception extends Exception {
+    public ArticleInEditingxception(String message) {
+        super(message);
+    }
+
+    public ArticleInEditingxception(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
