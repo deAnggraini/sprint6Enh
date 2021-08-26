470ea4913668467c5558abf1c29ffbe2357430b2
jumono <jumono@ogya.co.id>
2021-08-26 08:15:44 +0700

    update

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/WfApiClientException.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/exception/WfApiClientException.java
@@ -1,5 +1,8 @@
 package id.co.bca.pakar.be.doc.exception;
 
+/**
+ * 
+ */
 public class WfApiClientException extends Exception {
     public WfApiClientException(String message) {
         super(message);
