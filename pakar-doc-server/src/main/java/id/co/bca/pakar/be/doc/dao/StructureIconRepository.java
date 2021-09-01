ef9926ad9c6aec398d005092e9489541c2ac8c15
jumono <jumono@ogya.co.id>
2021-09-01 14:34:04 +0700

    add CacheEvict

--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureIconRepository.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureIconRepository.java
@@ -1,6 +1,7 @@
 package id.co.bca.pakar.be.doc.dao;
 
 import id.co.bca.pakar.be.doc.model.StructureIcons;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StructureIconRepository extends CrudRepository<StructureIcons, Long>{
     @Query("SELECT m FROM StructureIcons m WHERE m.structure.id=:structureId AND m.deleted IS FALSE")
+    @CacheEvict
     StructureIcons findByStructureId(Long structureId);
 }
--- pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureImageRepository.java
+++ pakar-doc-server/src/main/java/id/co/bca/pakar/be/doc/dao/StructureImageRepository.java
@@ -1,6 +1,7 @@
 package id.co.bca.pakar.be.doc.dao;
 
 import id.co.bca.pakar.be.doc.model.StructureImages;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StructureImageRepository extends CrudRepository<StructureImages, Long>{
     @Query("SELECT m FROM StructureImages m WHERE m.structure.id=:structureId AND m.deleted IS FALSE")
+    @CacheEvict
     StructureImages findByStructureId(@Param("structureId") Long structureId);
 }
